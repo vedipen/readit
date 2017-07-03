2d00f3cb99d4623a434ea70100dffe0ade8b99e4
Dipen Ved <vedipen@gmail.com>
2017-07-03 20:17:37 +0530

    Add link between views

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   match ':controller(/:action(/:id))', :via => :get
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'articles#index'
 end
