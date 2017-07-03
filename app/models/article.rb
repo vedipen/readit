45ef88b048911f15f6700f6fb2a2b37e9f6be10c
Dipen Ved <vedipen@gmail.com>
2017-07-03 18:15:04 +0530

    Article+Comments functionality added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  validates: :title, presence: true, length: { maximum:50 }
-  validates: :post, presence: true
+  has_many :comments
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :post, presence: true
 end
